22098924dee36bbcf5115f0d0d637f755c451213
leseizieme61-cyber <leseizieme61@gmail.com>
2026-02-11 12:02:18 +0100

    Update wsgi.py

--- mykarfour_app/wsgi.py
+++ mykarfour_app/wsgi.py
@@ -1,14 +1,6 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import notifications.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mykarfour_app.settings")
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(notifications.routing.websocket_urlpatterns)
-    ),
-})
+application = get_asgi_application()
